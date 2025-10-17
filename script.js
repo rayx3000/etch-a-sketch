f420bf4607c1d6329596a5b66309bf7e46cf6385
rayx <123456789+odin@users.noreply.github.com>
2025-10-17 11:47:48 +0800

    prepare for js

--- script.js
+++ script.js
@@ -1,12 +1,27 @@
 const setGridInput = document.getElementById("setGridInput");
 const setColor = document.getElementById("setColor");
 const setGridButton = document.getElementById("setGridButton");
+const container = document.getElementById("container");
 
-setGridButton.addEventListener("click", ()=> {
-    const gridInputValue = setGridInput.value;
-    const selectColorValue = setColor.value;
+setGridButton.addEventListener("click", () => {
+    const colorValue = setColor.value;
+    const gridInputValue = parseInt(setGridInput.value, 10);
+
+    if (isNaN(gridInputValue) || gridInputValue < 2 || gridInputValue > 200) {
+        console.log("Grid size must be a number between 2 and 200.");
+        return; 
+    }
+
+    if(!colorValue){
+        console.log("Please put a Color");
+        return;
+    }
+
+    const squaredValue = gridInputValue * gridInputValue
     
-    console.log(gridInputValue);
-    console.log(selectColorValue);
-    console.log("clicked");
+    for(let x = 0; x < squaredValue; x++){
+        console.log(x);
+    }
+
+    console.log(colorValue)
 });
